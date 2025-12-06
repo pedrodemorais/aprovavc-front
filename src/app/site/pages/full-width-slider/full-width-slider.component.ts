df37e514f98c7fc66c9e85365b6acd39ff3ac1fc
Your Name <you@example.com>
2025-12-06 18:45:54 -0300

    Alterado Layout minimalista

--- src/app/site/pages/full-width-slider/full-width-slider.component.ts
+++ src/app/site/pages/full-width-slider/full-width-slider.component.ts
@@ -8,19 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class FullWidthSliderComponent implements OnInit {
   slides = [
     {
-      image: 'assets/img/img11.jpg',
+      image: 'assets/img/img25.png',
       title: 'Soluções para Microempreendedores',
       description: 'Descubra ferramentas incríveis para facilitar sua gestão financeira e administrativa.',
       link: '/solucoes'
     },
     {
-      image: 'assets/img/img2.jpg',
+      image: 'assets/img/img25.png',
       title: 'Organize seu Negócio com Facilidade',
       description: 'Automatize tarefas e tenha controle total sobre seus clientes, vendas e estoque.',
       link: '/organizacao'
     },
     {
-      image: 'assets/img/img3.jpg',
+      image: 'assets/img/img25.png',
       title: 'Transforme sua Rotina',
       description: 'Aproveite tecnologia acessível para impulsionar seu negócio para o próximo nível.',
       link: '/tecnologia'
